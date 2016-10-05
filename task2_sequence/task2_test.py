be246661cecbf7a2c42600df92a1f88af37df71f
sfalexrog <sfalexrog@gmail.com>
2016-10-05 22:11:11 +0300

    Add additional tests for task 2
    Check edge cases like 7575, 1010, etc.

--- task2_sequence/task2_test.py
+++ task2_sequence/task2_test.py
@@ -15,7 +15,7 @@ def get_random_substring(length):
     return (firstpos + 1, substr)
 
 def check_position(number):
-    calc_position = task2.get_number_position(number)
+    calc_position = task2.get_number_position(number) - 1
     nrepr = str(number)
     num_at_pos = s[calc_position:calc_position + len(nrepr)]
     print('Expected number: {}, found number: {}'.format(nrepr, num_at_pos))
@@ -28,9 +28,21 @@ if __name__ == '__main__':
         n = randint(1, MAXNUM)
         if (not check_position(n)):
             print('Warning! Position calculation failed for {}'.format(n))
+    print('Running tests for edge cases')
+    for i in range(99):
+        for j in range(1, 5):
+            seq = str(i) * j
+            real_ans = s.find(seq) + 1
+            if real_ans < 0:
+                continue
+            ans = task2.get_sequence_pos(seq)
+            if ans != real_ans:
+                print('Failing sequence: {}, expected {}, got {}'.format(seq, real_ans, ans))
     fail_count = 0
     pass_count = 0
     TEST_NR = 9999999
+    FAIL_THRESHOLD = 1
+    failed_tests = []
     print('Testing if substring analysis is correct')
     for i in range(TEST_NR):
         rss = get_random_substring(randint(1, 20))
@@ -38,12 +50,15 @@ if __name__ == '__main__':
         # print('Expecting to find {} at {}...'.format(rss[1], rss[0]))
         if (ans != rss[0]):
             print('Test failed! Expected answer: {}, actual: {} (substring {})'.format(rss[0], ans, rss[1]))
+            failed_tests.append((rss[1], rss[0], ans)) # Substring, expected answer, actual answer
             fail_count += 1
+            if fail_count >= FAIL_THRESHOLD:
+                print('Failure limit hit; stopping...')
+                break
         else:
             pass_count += 1
         if (i % 100 == 0):
             print('Tests finished: {} out of {}'.format(i, TEST_NR))
     print('Testing finished; {} tests passed, {} failed'.format(pass_count, fail_count))
-    pass
-
-
+    for test in failed_tests:
+        print('Failed test case: substring {}, expected answer: {}, got answer: {}'.format(test[0], test[1], test[2]))
