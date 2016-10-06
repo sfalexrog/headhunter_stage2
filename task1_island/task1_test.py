64dbeda387a46ba4614927a674d52e4b235da736
sfalexrog <sfalexrog@gmail.com>
2016-10-06 16:43:24 +0300

    Add torture tests for task 1
    Seems to pass them just fine, just not sure if they pass correctly.

--- task1_island/task1_test.py
+++ task1_island/task1_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import task1
+from random import randint
 
 predef_isles = [
     ([[4, 5, 4],
@@ -22,4 +23,12 @@ if __name__ == '__main__':
         ans = task1.get_water(test_isle)
         if ans != test_ans:
             print('Test case failed! Expected answer: {}, actual answer: {}'.format(test_ans, ans))
-            
+    print('Generating random islands to check for crashes...')
+    TEST_NR = 999999
+    for i in range(TEST_NR):
+        x_size = randint(1, 50)
+        y_size = randint(1, 50)
+        isle = [[randint(1, 1000) for x in range(x_size)] for y in range(y_size)]
+        ans = task1.get_water(isle)
+        if i % 500 == 0:
+            print('Survived {} tests...'.format(i))
